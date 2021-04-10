fbe705e702369aa08ea926fd827a447a2dfaa6a2
bugrovms <www.maks-ky@list.ru>
2021-04-10 20:03:25 +0300

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,84 @@
-const gulp = require('gulp');
+const { src, dest, series, watch } = require('gulp');
+const del = require('del');
 const sass = require('gulp-sass');
-sass.compiler = require('node-sass');
-const browserSync = require('browser-sync').create();
-//const prefix = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify-es').default;
 const cleanCSS = require('gulp-clean-css');
-const fileinclude = require('gulp-file-include');
+const include = require('gulp-file-include');
+const webp = require('gulp-webp');
+const webpHTML = require('gulp-webp-html');
+const sync = require('browser-sync').create();
 
-const scssPath = './app/style/main.scss';
-const scssAllFilesPath = './app/style/sass/**/*';
-const cssPath = './build/css';
-
-const htmlAllFilesPath = './app/html/**/*';
-
-
-function style() {
-    return gulp.src(scssPath)
-        .pipe(sass())
-        // .pipe(prefix('last 2 versions'))
-        // .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(cleanCSS({level: 2}))
-        .pipe(gulp.dest(cssPath))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(browserSync.stream());
-
-}
+const buildFolder = 'docs'; //папка куда собирается проект (указываем docs, если нужен gitHubPage, дополнительно нужно указать в настройках gitHub)
 
 function html() {
-    return gulp.src(['./app/html/*.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./build'))
-    .pipe(browserSync.stream());
+    return src(['./app/html/**.html'])
+        .pipe(include())
+        //раскоментрировать если нужен webp внутри тега picture
+        // .pipe(webpHTML())
+        .pipe(dest(buildFolder))
 }
 
+function scss() {
+    return src('app/style/main.scss')
+        .pipe(sass())
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(dest(buildFolder + '/css'))
+        .pipe(sass().on('error', sass.logError))
+};
 
+// function js() {
+//     return src('app/js/main.js')
+//         .pipe(include({
+//             prefix: '@@'
+//         }))
+//         .pipe(uglify())
+//         .pipe(dest(buildFolder + '/js'))
+// };
 
-function watch() {
-    browserSync.init({
+function img() {
+    return src('app/img/**/*')
+        .pipe(imagemin({
+            interlaced: false,
+            progressive: false,
+            optimizationLevel: 3,
+            svgoPlugins: [
+                { removeViewBox: false }
+            ]
+        }))
+        //раскоментрировать если нужен webp
+        // .pipe(dest('build/img'))
+        // .pipe(webp({ quality: 70 }))
+        .pipe(dest(buildFolder + '/img'))
+};
+
+function fonts() {
+    return src('app/fonts/**/*')
+        .pipe(dest(buildFolder + '/fonts'))
+};
+
+function clear() {
+    return del(buildFolder)
+};
+
+function serve() {
+    sync.init({
         port: 3010,
         reloadOnRestart: true,
         server: {
-            baseDir: './build',
+            baseDir: buildFolder,
             directory: true
         }
     });
 
-    gulp.watch(htmlAllFilesPath, html);
-    gulp.watch(scssAllFilesPath, style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    watch('app/html/**/*.html', series(html)).on('change', sync.reload)
+    watch('app/scss/**/*.scss', series(scss)).on('change', sync.reload)
+    // watch('app/js/**/*.js', series(js)).on('change', sync.reload)
+    watch('app/img/**/*', series(img)).on('change', sync.reload)
+    watch('app/fonts/**/*', series(fonts)).on('change', sync.reload)
+};
 
-}
 
-exports.html = html;
-exports.style = style;
-exports.watch = watch;
+exports.build = series(clear, scss, img, fonts, html)
+exports.watch = series(clear, scss, img, fonts, html, serve)
+exports.clear = clear;
